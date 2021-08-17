b38e6b615e0d717ccfbc27538f8cef6e78f3b96d
Pete <peterorpete@me.com>
2021-08-17 14:15:27 +0100

    destructuring props

---
+++ React/destructuring-props/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+
+function Button(props) {
+ //Make sure this isnt in the return
+ const { className, children } = props;
+
+ return <button className={className}>{children}</button>;
+}
+
+const root = document.querySelector("#react-root");
+
+render(<Button className="primary">Login</Button>, root);
