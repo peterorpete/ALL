3d1f14416a0ca65c11038417a04f53a42cf7116d
Pete <peterorpete@me.com>
2021-08-19 12:08:18 +0100

    another example

--- Javascript/functions/default-function-parameters.js
+++ Javascript/functions/default-function-parameters.js
@@ -1,7 +1,19 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Default_parameters
+// *example 1
+////
 
 const leadSinger = (artist = "someone") => {
  console.log(`${artist} is the lead singer of Cold Play`);
 };
 
 leadSinger();
+
+////
+// *example 2
+////
+let goingToBuy = (food = "something") => {
+ console.log(`"I'm going to buy ${food} from the grocery shop"`);
+};
+
+goingToBuy("milk");
+goingToBuy();
