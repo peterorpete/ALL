e153d6476d623effd97910dde97e1ba1f03036f5
Pete <peterorpete@me.com>
2021-09-03 13:06:10 +0100

    reverse method

---
+++ Javascript/array/reverse().js
@@ -0,0 +1,11 @@
+////
+//* Sort method example 1 - sorting strings
+////
+const names = ["mario", "shaun", "chun-li", "yoshi", "luigi"];
+
+names.reverse();
+console.log(names);
+
+names.sort(); // if you sort first then reverse it will arrang alphabetically but other way around
+names.reverse();
+console.log(names);
