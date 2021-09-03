389c4693b051a2a7a23faff1bb66611518e74e1f
Pete <peterorpete@me.com>
2021-09-03 13:06:03 +0100

    find method

---
+++ Javascript/array/find().js
@@ -0,0 +1,11 @@
+////
+//* Map method example 1
+// return the first true value from the function
+////
+const scores = [20, 10, 56, 15, 76, 85, 5];
+
+const firstHighScore = scores.find((score) => {
+ return score > 50; // either be true or false. When the first true occurs the loop stops so only one result
+});
+
+console.log(firstHighScore); //56
