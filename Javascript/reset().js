53f7ecebdcb07b72fa8382cca52dade8f80003f1
Pete <peterorpete@me.com>
2021-09-07 23:51:18 +0100

    reset

---
+++ Javascript/reset().js
@@ -0,0 +1,14 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reset
+
+////
+//* example where it clears the forms input after submitted
+// #removes #delete #clear #form
+////
+addForm.addEventListener("submit", (e) => {
+ e.preventDefault();
+ const todo = addForm.add.value.trim();
+ if (todo.length) {
+  generateTemplate(todo);
+ }
+ addForm.reset(); // clears the previous forms input
+});
