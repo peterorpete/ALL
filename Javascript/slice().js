9da1c9fd4da7705ce57ba4fa6f1b37d5daf57b69
Pete <peterorpete@me.com>
2021-09-08 18:03:43 +0100

    slice

---
+++ Javascript/slice().js
@@ -0,0 +1,11 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+// The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
+
+////
+//* Simple string slice example
+//# Find a set part of a string
+////
+
+// slice - returns a "slice" of a string
+"pancake".slice(3); //"cake" - slice from index 3 onwards
+"pancake".slice(0, 3); //"pan" - slice from index 0 up to index 3
