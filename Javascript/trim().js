8f18ec2c1cc0f6cdcd5dcfd0e307617b3929ea1e
Pete <peterorpete@me.com>
2021-09-08 18:04:16 +0100

    trim

---
+++ Javascript/trim().js
@@ -0,0 +1,11 @@
+//https://www.w3schools.com/jsref/jsref_trim_string.asp
+
+////
+//* simple example
+//# how to remove spaces
+////
+
+let str = "       Hello World!        ";
+str.trim(); // Returns "Hello World!"
+
+Comment;
