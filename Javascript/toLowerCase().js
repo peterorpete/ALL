5a78504c90926defe5cab2181696f833fccd3a43
Pete <peterorpete@me.com>
2021-09-08 18:03:54 +0100

    tolowercase

---
+++ Javascript/toLowerCase().js
@@ -0,0 +1,37 @@
+//https://www.w3schools.com/jsref/jsref_tolowercase.asp
+
+////
+//* simple example
+//# how to convert text to lowercase
+////
+
+let str = "Hello World!";
+str.toLowerCase(); //hello world!
+comt;
+for (let index = 0; index < array.length; index++) {
+ const element = array[index];
+}
+for (let index = 0; index < array.length; index++) {
+ const element = array[index];
+}
+comt;
+ct;
+ct56;
+for (let index = 0; index < array.length; index++) {
+ const element = array[index];
+}
+ct56;
+ct56
+console.log(
+  for
+);
+for (let 
+   = 0; 
+   < array.length; 
+  ++) {
+  const element = array[
+    
+  ];
+  
+}
+ct56
