61a69f9cca45cf5b9c148e76d2b2760167b85ab6
Pete <peterorpete@me.com>
2021-09-10 15:57:20 +0100

    switch

---
+++ Javascript/switch_statement.js
@@ -0,0 +1,21 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
+// The switch statement evaluates an expression, matching the expression's value to a case clause, and executes statements associated with that case, as well as statements in cases that follow the matching case.
+
+////
+//* ! Fruit switch statement example
+//# if is this otherwise do this
+////
+
+const expr = "Papayas";
+switch (expr) {
+ case "Oranges":
+  console.log("Oranges are $0.59 a pound.");
+  break;
+ case "Mangoes":
+ case "Papayas":
+  console.log("Mangoes and papayas are $2.79 a pound.");
+  // expected output: "Mangoes and papayas are $2.79 a pound."
+  break;
+ default:
+  console.log(`Sorry, we are out of ${expr}.`);
+}
