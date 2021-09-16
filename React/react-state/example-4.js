ab5861e2e0303dd01d0ac9f6d2a479833bd9c6be
Pete <peterorpete@me.com>
2021-09-16 16:09:42 +0100

    react state example

---
+++ React/react-state/example-4.js
@@ -0,0 +1,34 @@
+////
+// * React state example
+// # React state example
+// From this lesson: https://scrimba.com/learn/learnreact/react-todo-app-phase-4-cGreKTQ
+////
+
+/*
+In the previous iteration of this todo list app, we pulled in todos data from a JSON file and mapped over it to display the todo items.
+
+Eventually we'll want to be able to modify the data, which will only happen if we've "loaded" the data in to the component's state
+
+Challenge: Change the <App /> component into a stateful class component and load the imported `todosData` into state.
+*/
+
+import React from "react";
+import TodoItem from "./TodoItem";
+import todosData from "./todosData";
+
+class App extends React.Component {
+ constructor() {
+  super();
+  this.state = {
+   todos: todosData,
+  };
+ }
+
+ render() {
+  const todoItems = this.state.todos.map((item) => <TodoItem key={item.id} item={item} />);
+
+  return <div className="todo-list">{todoItems}</div>;
+ }
+}
+
+export default App;
