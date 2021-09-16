47694d4a66a53fce24e337a37c83e6d81fefff39
Pete <peterorpete@me.com>
2021-09-16 13:00:21 +0100

    neew array for loop example

--- Javascript/loop-for.js
+++ Javascript/loop-for.js
@@ -24,3 +24,16 @@ let cards = [1, 6, 19, 47, 43];
 for (let i = 0; i < cards.length; i++) {
  console.log(cards[i]);
 }
+
+////
+// * example 3 - Loop through arrays to make a sentence
+//# sentence from array, make a array one string
+////
+let sentence = ["Hello ", "my ", "name ", "is ", "Peter"];
+let greetingEl = document.getElementById("greeting-el");
+
+// Render the sentence in the greetingEl paragraph using a for loop and .textContent
+
+for (let i = 0; i < sentence.length; i++) {
+ greetingEl.textContent += `${sentence[i]} `;
+}
